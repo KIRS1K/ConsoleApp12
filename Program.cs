a0421f60d1c6ffd2ca0eea780fbf6093e518f56d
KIRS1K <ilya.kirsnov@bk.ru>
2024-09-03 21:06:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CustomExceptionExample
+{
+    public class MyCustomException : Exception
+    {
+        public MyCustomException(string message) : base(message)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Exception[] exceptions = new Exception[]
+            {
+                new DivideByZeroException("Деление на ноль"),
+                new NullReferenceException("Ссылка на объект не установлена"),
+                new ArgumentException("Неверный аргумент"),
+                new InvalidOperationException("Недопустимая операция"),
+                new MyCustomException("Мое собственное исключение")
+            };
+            foreach (var ex in exceptions)
+            {
+                try
+                { 
+
+                    throw ex;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Поймано исключение: {e.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("Блок finally выполняется.");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
